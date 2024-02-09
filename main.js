fafa54e0e9963d9dc75f7fd2cdd8af74071e4185
masudrana2779 <masudrana2779@gmail.com>
2024-02-09 17:26:50 +0600

    slider try to doen

---
+++ main.js
@@ -0,0 +1,39 @@
+var photos = [
+  "./img/post_img_1.png",
+  "./img/post_img_2.png",
+  "./img/post_img_3.png",
+];
+var imgTag = document.getElementById("img");
+
+var count = 0;
+
+function prev() {
+  count--;
+  if (count < 0) {
+    count = photos.length - 1;
+    imgTag.src = photos[count];
+  } else {
+    imgTag.src = photos[count];
+  }
+}
+
+function next() {
+  count++;
+  if (count >= photos.length) {
+    count = 0;
+    imgTag.src = photos[count];
+  } else {
+    imgTag.src = photos[count];
+  }
+}
+
+var go;
+function autoplay() {
+  go = setInterval(() => {
+    next();
+  }, 1000);
+}
+
+function stopAutoplay() {
+    clearInterval(go);
+}
