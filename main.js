394272d7e59e77547a734d4cc7f8688af0d6987d
masudrana2779 <masudrana2779@gmail.com>
2024-02-17 00:43:32 +0600

    task - 13

--- main.js
+++ main.js
@@ -1,47 +1,29 @@
-var photos = [
-  "./img/post_img_1.png",
-  "./img/post_img_2.png",
-  "./img/post_img_3.png",
-];
-var imgTag = document.getElementById("img");
-
-var count = 0;
-
-function prev() {
-  count--;
-  if (count < 0) {
-    count = photos.length - 1;
-    imgTag.src = photos[count];
-  } else {
-    imgTag.src = photos[count];
-  }
-} 
-
-function next() {
-  count++;
-  if (count >= photos.length) {
-    count = 0;
-    imgTag.src = photos[count];
-  } else {
-    imgTag.src = photos[count];
-  }
-}
-
-var go;
-function autoplay() {
-  go = setInterval(() => {
-    next();
-  }, 3000);
-}
-
-function stopAutoplay() {
-    clearInterval(go);
-}
+// dragstart
+// dragend
+// drag
+// dragenter
+// dragleave
+// dragover 
+// drop
 
 
-var textarea = document.getElementById('test');
+const box = document.querySelector(".div");
+const p = document.querySelector(".text");
 
-textarea.addEventListener('keydown', (e) => {
-  console.log('keydown = ' + e.key);
-  console.log(e);
+p.addEventListener("dragstart", function (e) {
+  e.dataTransfer.setData("Text", e.target.className);
+});
+
+box.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+box.addEventListener("drop", function (e) {
+  let id = e.dataTransfer.getData("Text");
+  box.appendChild(document.getElementsByClassName(id)[0]);
+  e.preventDefault();
+});
+
+box.addEventListener("dragend", function (e) {
+  e.target.style.fontSize = "20px";
 });
